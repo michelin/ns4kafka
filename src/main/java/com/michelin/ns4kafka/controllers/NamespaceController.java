0b013185b3bb1fd72e145062ed686341821dfe0b
twobeeb <chanaud.julien@gmail.com>
2020-11-04 22:35:35 +0100

    Initial Commit

--- src/main/java/com/michelin/ns4kafka/controllers/NamespaceController.java
+++ src/main/java/com/michelin/ns4kafka/controllers/NamespaceController.java
@@ -14,9 +14,10 @@ import javax.annotation.security.RolesAllowed;
 import javax.naming.Name;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Secured(SecurityRule.IS_ANONYMOUS)
-@Controller("api/namespace")
+@Controller("/api/namespace")
 public class NamespaceController {
     @Get
     public List<String> list(){
@@ -24,13 +25,21 @@ public class NamespaceController {
     }
 
     @Get("{namespace}")
-    public String display(String namespace){
-        return "1";
+    public Namespace display(String namespace){
+        return generateFakeNS(namespace);
     }
 
     @Post("{namespace}")
     public HttpResponse create(String namespace){
         return HttpResponse.status(HttpStatus.CONFLICT,"Resource already exists. Use PUT instead.")
                 .body("Use PUT\n");
+    }
+
+    private Namespace generateFakeNS(String name){
+        Namespace n = new Namespace();
+        n.setName(name);
+        n.setAdminLdapGroup("GP-FAKE-ADMIN");
+        n.setQuotas(Map.of("cluster1","5Go","cluster2","2Go"));
+        return n;
     }
 }
--- src/main/java/com/michelin/ns4kafka/models/Namespace.java
+++ src/main/java/com/michelin/ns4kafka/models/Namespace.java
@@ -1,13 +1,12 @@
 package com.michelin.ns4kafka.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 import java.util.Map;
 
 @Data
 public class Namespace {
-    private String namespace;
+    private String name;
     private String adminLdapGroup;
     private Map<String, String> quotas;
 }
