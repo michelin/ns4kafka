2a1a9d2e060cc4b85d310a71e39c6fc0c91cc8ea
Twobeeb <chanaud.julien@gmail.com>
2021-03-14 01:11:51 +0100

    Authent using Micronaut LoginController

---
+++ src/main/java/com/michelin/ns4kafka/security/gitlab/package-info.java
@@ -0,0 +1,8 @@
+@Configuration
+@Requires(property = "micronaut.security.gitlab.enabled", notEquals = StringUtils.FALSE)
+package com.michelin.ns4kafka.security.gitlab;
+
+import io.micronaut.context.annotation.Configuration;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.util.StringUtils;
+
