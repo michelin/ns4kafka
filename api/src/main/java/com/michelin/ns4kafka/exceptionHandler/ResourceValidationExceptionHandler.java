32e49758de8b6bebebb59a174be8db52b52a128d
Adrien Alexandre <alexandreadrien@orange.fr>
2021-03-19 17:45:43 +0100

    First try to create a dedicated exception handler

---
+++ api/src/main/java/com/michelin/ns4kafka/exceptionHandler/ResourceValidationExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.michelin.ns4kafka.exceptionHandler;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.server.exceptions.ExceptionHandler;
+import javax.inject.Singleton;
+
+import com.michelin.ns4kafka.validation.ResourceValidationException;
+
+@Produces
+@Singleton
+@Requires(classes = { ResourceValidationException.class, ExceptionHandler.class })
+public class ResourceValidationExceptionHandler implements ExceptionHandler<ResourceValidationException, HttpResponse> {
+
+    @Override
+    public HttpResponse handle(HttpRequest request, ResourceValidationException exception) {
+        return HttpResponse.badRequest().body(new ResourceCreationError("Message validation failed",
+                resourceValidationException.getValidationErrors()));
+    }
+}
