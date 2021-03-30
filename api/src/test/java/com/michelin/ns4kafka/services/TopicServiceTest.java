68897bb1050adca55f4dfce30ae6e5a3e0182351
Adrien Alexandre <alexandreadrien@orange.fr>
2021-03-30 18:53:41 +0200

    Add test to the Topic Service

--- api/src/test/java/com/michelin/ns4kafka/services/TopicServiceTest.java
+++ api/src/test/java/com/michelin/ns4kafka/services/TopicServiceTest.java
@@ -3,7 +3,13 @@ package com.michelin.ns4kafka.services;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.michelin.ns4kafka.models.AccessControlEntry;
+import com.michelin.ns4kafka.models.AccessControlEntry.AccessControlEntrySpec;
+import com.michelin.ns4kafka.models.AccessControlEntry.Permission;
+import com.michelin.ns4kafka.models.AccessControlEntry.ResourcePatternType;
+import com.michelin.ns4kafka.models.AccessControlEntry.ResourceType;
 import com.michelin.ns4kafka.models.Namespace;
 import com.michelin.ns4kafka.models.ObjectMeta;
 import com.michelin.ns4kafka.models.Topic;
@@ -21,6 +27,8 @@ public class TopicServiceTest {
 
     @Mock
     TopicRepository topicRepository;
+    @Mock
+    AccessControlEntryService acls;
 
     @InjectMocks
     TopicService topicService;
@@ -32,12 +40,61 @@ public class TopicServiceTest {
                   .cluster("local")
                   .build())
         .build();
+    List<AccessControlEntry> aceList = List.of(AccessControlEntry.builder()
+                                            .spec(AccessControlEntrySpec.builder()
+                                                  .resourceType(ResourceType.TOPIC)
+                                                  .resourcePatternType(ResourcePatternType.PREFIXED)
+                                                  .permission(Permission.OWNER)
+                                                  .grantedTo("test")
+                                                  .resource("test_")
+                                                  .build())
+                                            .build());
+    Topic topic = Topic.builder()
+        .metadata(ObjectMeta.builder()
+                  .name("test_name")
+                  .build())
+        .build();
 
     @Test
-    void findAllForNamespaceEmptyTopic(){
+    void findAllForNamespaceEmptyTopic() {
+
         when(topicRepository.findAllForCluster(ns.getMetadata().getCluster()))
             .thenReturn(List.of());
+        when(acls.findAllGrantedToNamespace(ns)).thenReturn(List.of());
+
         List<Topic> actual = topicService.findAllForNamespace(ns);
         Assertions.assertTrue(actual.isEmpty());
     }
+
+    @Test
+    void findAllForNamespaceListOfTopic() {
+
+        when(topicRepository.findAllForCluster(ns.getMetadata().getCluster()))
+            .thenReturn(List.of(topic));
+        when(acls.findAllGrantedToNamespace(ns)).thenReturn(aceList);
+
+        List<Topic> actual = topicService.findAllForNamespace(ns);
+        Assertions.assertFalse(actual.isEmpty());
+    }
+
+    @Test
+    void findByNameExist() {
+        when(topicRepository.findAllForCluster(ns.getMetadata().getCluster()))
+            .thenReturn(List.of(topic));
+        when(acls.findAllGrantedToNamespace(ns)).thenReturn(aceList);
+
+        Optional<Topic> actual = topicService.findByName(ns,"test_name");
+        Assertions.assertFalse(actual.isEmpty());
+    }
+
+    @Test
+    void findByNameNotExist() {
+        when(topicRepository.findAllForCluster(ns.getMetadata().getCluster()))
+            .thenReturn(List.of());
+        when(acls.findAllGrantedToNamespace(ns)).thenReturn(List.of());
+
+        Optional<Topic> actual = topicService.findByName(ns,"test_name");
+        Assertions.assertTrue(actual.isEmpty());
+    }
+
 }
