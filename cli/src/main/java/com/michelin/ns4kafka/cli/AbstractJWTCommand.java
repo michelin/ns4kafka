ae0a7ff1ae5c7435386b113d166cde81a69e3984
Adrien Alexandre <alexandreadrien@orange.fr>
2021-04-15 12:07:35 +0200

    Add subclass to get resource

--- cli/src/main/java/com/michelin/ns4kafka/cli/AbstractJWTCommand.java
+++ cli/src/main/java/com/michelin/ns4kafka/cli/AbstractJWTCommand.java
@@ -4,10 +4,23 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import javax.inject.Inject;
+
+import com.michelin.ns4kafka.cli.client.ResourceDefinitionClient;
+import com.michelin.ns4kafka.cli.models.ResourceDefinition;
 
 import io.micronaut.core.io.IOUtils;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 
 public abstract class AbstractJWTCommand {
+
+    @Inject
+    ManageResource manageResource;
 
     public String getJWT() {
         BufferedReader in;
@@ -21,6 +34,32 @@ public abstract class AbstractJWTCommand {
             System.out.println("Please login first.");
         }
         return jwt;
+    }
+
+    public static class ManageResource {
+
+        @Inject
+        ResourceDefinitionClient resourceClient;
+
+        private  List<ResourceDefinition> getListResourceDefinition() {
+            //TODO Add Cache to reduce the number of http requests
+            return resourceClient.getResource();
+        }
+
+        public Optional<ResourceDefinition> getResourceDefinitionFromKind(String kind) {
+            List<ResourceDefinition> resourceDefinitions = getListResourceDefinition();
+            return resourceDefinitions.stream()
+                .filter(resource -> resource.getKind().equals(kind))
+                .findFirst();
+        }
+        public Optional<ResourceDefinition> getResourceDefinitionFromName(String name) {
+            List<ResourceDefinition> resourceDefinitions = getListResourceDefinition();
+            return resourceDefinitions.stream()
+                .filter(resource -> resource.getNames().contains(name))
+                .findFirst();
+
+
+
     }
 
 }
