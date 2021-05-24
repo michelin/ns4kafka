7f1bb8acaa77c4abc8889990e4a4f4a44b3fb15a
Sebastien Viale <sebastien.viale63@gmail.com>
2021-05-24 15:30:49 +0200

    add synchronize with connects

--- api/src/main/java/com/michelin/ns4kafka/controllers/SynchronizeNamespaceController.java
+++ api/src/main/java/com/michelin/ns4kafka/controllers/SynchronizeNamespaceController.java
@@ -1,19 +1,26 @@
 package com.michelin.ns4kafka.controllers;
 
 import com.michelin.ns4kafka.models.AccessControlEntry;
+import com.michelin.ns4kafka.models.Connector;
 import com.michelin.ns4kafka.models.Namespace;
 import com.michelin.ns4kafka.models.Topic;
 import com.michelin.ns4kafka.security.ResourceBasedSecurityRule;
 import com.michelin.ns4kafka.services.AccessControlEntryService;
+import com.michelin.ns4kafka.services.KafkaConnectService;
 import com.michelin.ns4kafka.services.SynchronizeNamespaceService;
 import com.michelin.ns4kafka.services.TopicService;
+import com.michelin.ns4kafka.services.connect.client.KafkaConnectClient;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.http.annotation.Status;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
@@ -29,13 +36,17 @@ public class SynchronizeNamespaceController extends NamespacedResourceController
 
     @Inject
     TopicService topicService;
-    
+
+    @Inject
+    KafkaConnectService kafkaConnectService;
+
     @Inject
     SynchronizeNamespaceService synchronizeNamespaceService;
 
     @Post
-    public List<Topic> synchronize(String namespace, @QueryValue(defaultValue = "false") boolean dryrun) 
+    public List<Object> synchronize(String namespace, @QueryValue(defaultValue = "false") boolean dryrun)
             throws ExecutionException, InterruptedException, TimeoutException {
+        
         Namespace ns = getNamespace(namespace);
 
         if (ns == null)
@@ -45,15 +56,15 @@ public class SynchronizeNamespaceController extends NamespacedResourceController
         List<AccessControlEntry> accessControlEntries = accessControlEntryService.findAllGrantedToNamespace(ns);
 
         // Get list of ns4kfk topic names
-        List<Topic> topics = topicService.findAllForNamespace(ns);
-        List<String> topicNames = topics
+        List<Topic> existingTopics = topicService.findAllForNamespace(ns);
+        List<String> topicNames = existingTopics
                 .stream()
-                .map(topic -> 
+                .map(topic ->
                         topic.getMetadata().getName()).collect(Collectors.toList()
                 );
 
         // Filter the list to create between existing ns4kfk topics and cluster topics
-        List<String> topicToCreate = accessControlEntries                
+        List<String> topicToCreate = accessControlEntries
                 .stream()
                 .filter(accessControlEntry -> {
                             if (accessControlEntry.getSpec().getResourceType().equals(AccessControlEntry.ResourceType.TOPIC)
@@ -67,15 +78,37 @@ public class SynchronizeNamespaceController extends NamespacedResourceController
                 .map(accessControlEntry -> accessControlEntry.getSpec().getResource())
                 .collect(Collectors.toList());
 
-        // Build Topic object for topics to create in ns4kfk
-        List<Topic> list = synchronizeNamespaceService.buildTopics(topicToCreate, namespace);
-        
-        // Create ns4kfk topics
-        if(dryrun) {
-            return list;
-        }
-        list.stream().forEach(topic -> topicService.create(topic));
+        List<String> connectPatternToCreate = accessControlEntries
+                .stream()
+                .filter(accessControlEntry -> {
+                            if (accessControlEntry.getSpec().getResourceType().equals(AccessControlEntry.ResourceType.CONNECT)
+                                    && accessControlEntry.getSpec().getResourcePatternType().equals(AccessControlEntry.ResourcePatternType.PREFIXED)
+                            ) {
+                                return true;
+                            }
+                            return false;
+                        }
+                )
+                .map(accessControlEntry -> accessControlEntry.getSpec().getResource())
+                .collect(Collectors.toList());
+        List<Connector> connectors =  synchronizeNamespaceService.buildConnectList(connectPatternToCreate);
 
+        // Build Topic object for topics to create in ns4kfk
+        List<Topic> topics = synchronizeNamespaceService.buildTopics(topicToCreate, namespace);
+
+        // Create ns4kfk topics
+        if (!dryrun) {
+            HttpResponse.noContent();
+        }
+        topics.stream().forEach(topic -> topicService.create(topic));
+        System.out.println(connectors);
+        System.out.println(kafkaConnectService);
+        System.out.println(ns);
+        connectors.stream().forEach(connector -> kafkaConnectService.createOrUpdate(ns, connector));
+        
+        List<Object> list = new ArrayList<>();
+        list.addAll(topics);
+        list.addAll(connectors);
         return list;
     }
 
--- api/src/main/java/com/michelin/ns4kafka/services/SynchronizeNamespaceService.java
+++ api/src/main/java/com/michelin/ns4kafka/services/SynchronizeNamespaceService.java
@@ -1,10 +1,13 @@
 package com.michelin.ns4kafka.services;
 
+import com.michelin.ns4kafka.models.Connector;
 import com.michelin.ns4kafka.models.ObjectMeta;
 import com.michelin.ns4kafka.models.Topic;
 import com.michelin.ns4kafka.repositories.NamespaceRepository;
 import com.michelin.ns4kafka.repositories.TopicRepository;
+import com.michelin.ns4kafka.services.connect.client.KafkaConnectClient;
 import com.michelin.ns4kafka.services.executors.KafkaAsyncExecutorConfig;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.ConfigEntry;
 import org.apache.kafka.clients.admin.TopicDescription;
@@ -15,6 +18,7 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
@@ -31,6 +35,9 @@ public class SynchronizeNamespaceService {
     private Admin adminClient;
 
     private final KafkaAsyncExecutorConfig kafkaAsyncExecutorConfig;
+    
+    @Inject
+    private KafkaConnectClient kafkaConnectClient;
 
     public SynchronizeNamespaceService(KafkaAsyncExecutorConfig kafkaAsyncExecutorConfig) throws MalformedURLException {
         this.kafkaAsyncExecutorConfig = kafkaAsyncExecutorConfig;
@@ -43,6 +50,15 @@ public class SynchronizeNamespaceService {
         return this.adminClient;
     }
 
+    /**
+     * 
+     * @param topics
+     * @param namespace
+     * @return
+     * @throws InterruptedException
+     * @throws ExecutionException
+     * @throws TimeoutException
+     */
     public List<Topic> buildTopics(List<String> topics, String namespace) throws InterruptedException, ExecutionException, TimeoutException {
 
         Map<String, TopicDescription> topicDescriptions = getAdminClient().describeTopics(topics).all().get();
@@ -81,8 +97,42 @@ public class SynchronizeNamespaceService {
                 )
                 .collect(Collectors.toList());
        }
-    
-    
-    
+
+    /**
+     * 
+     * @param patterns
+     * @return
+     */
+    public List<Connector> buildConnectList(List<String> patterns){
+           List<Connector> connectors = new ArrayList<>();
+           String[] patternArray = new String[patterns.size()];
+           patterns.toArray(patternArray);
+           kafkaAsyncExecutorConfig.getConnects().forEach(
+                   (connectCluster, connectConfig) ->
+                           connectors.addAll(kafkaConnectClient.listAll(kafkaAsyncExecutorConfig.getName(), connectCluster)
+                                   .values()
+                                   .stream()
+                                   .map(connectorStatus -> {
+                                       return Connector.builder()
+                                               .metadata(ObjectMeta.builder()
+                                                       // Any other metadata is not usefull for this process
+                                                       .name(connectorStatus.getInfo().name())
+                                                       .cluster(kafkaAsyncExecutorConfig.getName())
+                                                       .build())
+                                               .spec(Connector.ConnectorSpec.builder()
+                                                       .connectCluster(connectCluster)
+                                                       .config(connectorStatus.getInfo().config())
+                                                       .build())
+                                               .build();
+
+                                   })
+                                   .filter(connector -> StringUtils.startsWithAny(
+                                           connector.getMetadata().getName(), patternArray)
+                                   )
+                                   .collect(Collectors.toList()))
+           );
+           return connectors;
+       }
+       
 }
 
