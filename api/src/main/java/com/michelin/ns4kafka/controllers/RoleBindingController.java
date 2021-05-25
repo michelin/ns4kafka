a6ba24b54fb4fb8b99d05bc85716c87d3d0b1a3c
twobeeb <chanaud.julien@gmail.com>
2021-05-26 00:43:25 +0200

    added creationTimestamp during apply to all resources

--- api/src/main/java/com/michelin/ns4kafka/controllers/RoleBindingController.java
+++ api/src/main/java/com/michelin/ns4kafka/controllers/RoleBindingController.java
@@ -14,6 +14,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -46,6 +48,8 @@ public class RoleBindingController extends NamespacedResourceController {
 
         // fill with cluster name
         Namespace ns = getNamespace(namespace);
+        // augment
+        rolebinding.getMetadata().setCreationTimestamp(Date.from(Instant.now()));
         rolebinding.getMetadata().setCluster(ns.getMetadata().getCluster());
         rolebinding.getMetadata().setNamespace(namespace);
 
