f643e7ade3a4e532e7fdbf8d3643cfb57fc5b322
Julien Chanaud <chanaud.julien@gmail.com>
2021-05-27 22:59:45 +0200

    Delete build-native.bat

--- build-native.bat
+++
@@ -1,4 +0,0 @@
-native-image -cp H:\projects\ns4kafka\cli\build\libs\kafkactl-0.1.jar \
-  -H:+ReportUnsupportedElementsAtRuntime -H:Name=kafkactl \
-  -H:IncludeResourceBundles=org.ocpsoft.prettytime.i18n.Resources \
-  --no-server com.michelin.ns4kafka.cli.KafkactlCommand
