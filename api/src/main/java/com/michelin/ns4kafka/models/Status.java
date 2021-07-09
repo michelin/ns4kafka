bd9f1512eadc63958348310f173098fe1dffa76a
Adrien Alexandre <alexandreadrien@orange.fr>
2021-07-09 10:58:45 +0200

    Creation of the status classes to manage errors

---
+++ api/src/main/java/com/michelin/ns4kafka/models/Status.java
@@ -0,0 +1,56 @@
+package com.michelin.ns4kafka.models;
+
+import io.micronaut.core.annotation.Introspected;
+import lombok.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Introspected
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Status {
+    private final String apiVersion = "v1";
+    private final String kind = "Status";
+
+    @Valid
+    private ObjectMeta metadata;
+
+    private StatusPhase status;
+
+    private String message;
+    private String reason;
+
+    private StatusDetails details;
+
+    private int code;
+
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    public static class StatusDetails {
+        private String name;
+        private String kind;
+        private List<StatusCauses> causes;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    public static class StatusCauses {
+        private String message;
+        private String reason;
+        private String field;
+    }
+
+    public enum StatusPhase {
+        Success,
+        Failed
+    }
+
+}
