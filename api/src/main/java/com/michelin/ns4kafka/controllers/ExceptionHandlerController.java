b0dccacf7ec772f1fc7aa68118bed6fb59b0bec0
Adrien Alexandre <alexandreadrien@orange.fr>
2021-07-09 11:24:22 +0200

    Start changing the error Handler

--- api/src/main/java/com/michelin/ns4kafka/controllers/ExceptionHandlerController.java
+++ api/src/main/java/com/michelin/ns4kafka/controllers/ExceptionHandlerController.java
@@ -1,5 +1,8 @@
 package com.michelin.ns4kafka.controllers;
 
+import com.michelin.ns4kafka.models.Status;
+import com.michelin.ns4kafka.models.Status.StatusPhase;
+
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
@@ -10,10 +13,17 @@ import io.micronaut.http.hateoas.JsonError;
 public class ExceptionHandlerController {
 
     @Error(global = true)
-    public HttpResponse<JsonError> error(HttpRequest<?> request, ResourceValidationException exception) {
-        String message = exception.getValidationErrors().toString();
-        return HttpResponse.badRequest()
-                .body(new JsonError("Validation failed: " + message));
+    public HttpResponse<Status> error(HttpRequest<?> request, ResourceValidationException exception) {
+        var status = Status.builder()
+            .status(StatusPhase.Failed)
+            .message("Validation errors for %s %s")
+            .reason("ValidationError")
+            .details(null)
+            .code(HttpResponse.unprocessableEntity().code())
+            .build();
+
+        return HttpResponse.unprocessableEntity()
+                .body(status);
     }
 
     @Error(global = true)
