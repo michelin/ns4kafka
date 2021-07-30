1bdbe42ade9e0cabdf2633749f0eb3ab0bfd4d7d
Adrien Alexandre <alexandreadrien@orange.fr>
2021-07-30 15:44:28 +0200

    Add AuditLog publisher in ResourceController class

--- api/src/main/java/com/michelin/ns4kafka/controllers/ResourceController.java
+++ api/src/main/java/com/michelin/ns4kafka/controllers/ResourceController.java
@@ -1,12 +1,48 @@
 package com.michelin.ns4kafka.controllers;
 
+import com.michelin.ns4kafka.models.ObjectMeta;
+import io.micronaut.context.event.ApplicationEventPublisher;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.security.utils.SecurityService;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.inject.Inject;
+import java.time.Instant;
+import java.util.Date;
 
 public abstract class ResourceController {
+
+    @Inject
+    SecurityService securityService;
+
+    @Inject
+    ApplicationEventPublisher applicationEventPublisher;
 
     public final String statusHeaderName = "X-Ns4kafka-Result";
 
     public <T> HttpResponse<T> formatHttpResponse(T body, ApplyStatus status) {
         return HttpResponse.ok(body).header(statusHeaderName, status.toString());
     }
+
+    public void sendEventLog(String kind, ObjectMeta metadata, Object operation, String before, String after) {
+        var auditLog = new AuditLog(securityService, Date.from(Instant.now()), kind, metadata, operation, before, after);
+        applicationEventPublisher.publishEvent(auditLog);
+    }
+
+    @Builder
+    @Data
+    public static class AuditLog {
+
+        private SecurityService user;
+        private Date date;
+        private String kind;
+        private ObjectMeta metadata;
+        private Object operation;
+        private String before;
+        private String after;
+
+
+    }
+
 }
