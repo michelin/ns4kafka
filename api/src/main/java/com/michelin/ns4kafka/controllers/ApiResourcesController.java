e329a4f9ce239828d26cea57600f299cebeab82e
twobeeb <chanaud.julien@gmail.com>
2021-08-30 10:41:48 +0200

    Configurable column definitions

--- api/src/main/java/com/michelin/ns4kafka/controllers/ApiResourcesController.java
+++ api/src/main/java/com/michelin/ns4kafka/controllers/ApiResourcesController.java
@@ -14,48 +14,50 @@ import java.util.List;
 @RolesAllowed(SecurityRule.IS_ANONYMOUS)
 @Controller("/api-resources")
 public class ApiResourcesController {
-
-    @Get
-    public List<ResourceDefinition> list() {
-        return List.of(
-                // TODO generate from Resource classes
-                // Namespaced Resources
-                ResourceDefinition.builder()
-                        .kind("AccessControlEntry")
-                        .namespaced(true)
-                        .synchronizable(false)
-                        .path("acls")
-                        .names(List.of("acls", "acl", "ac"))
-                        .build(),
-                ResourceDefinition.builder()
-                        .kind("Connector")
-                        .namespaced(true)
-                        .synchronizable(true)
-                        .path("connects")
-                        .names(List.of("connects", "connect", "co"))
-                        .build(),
-                ResourceDefinition.builder()
-                        .kind("RoleBinding")
-                        .namespaced(true)
-                        .synchronizable(false)
-                        .path("role-bindings")
-                        .names(List.of("rolebindings", "rolebinding", "rb"))
-                        .build(),
-                ResourceDefinition.builder()
-                        .kind("Topic")
-                        .namespaced(true)
-                        .synchronizable(true)
-                        .path("topics")
-                        .names(List.of("topics", "topic", "to"))
-                        .build(),
-                // Non-Namespaced Resources
-                ResourceDefinition.builder()
-                        .kind("Namespace")
+    public static final ResourceDefinition ACL = ResourceDefinition.builder()
+            .kind("AccessControlEntry")
+            .namespaced(true)
+            .synchronizable(false)
+            .path("acls")
+            .names(List.of("acls", "acl", "ac"))
+            .build();
+    public static final ResourceDefinition CONNECTOR = ResourceDefinition.builder()
+            .kind("Connector")
+            .namespaced(true)
+            .synchronizable(true)
+            .path("connects")
+            .names(List.of("connects", "connect", "co"))
+            .build();
+    public static final ResourceDefinition ROLE_BINDING = ResourceDefinition.builder()
+            .kind("RoleBinding")
+            .namespaced(true)
+            .synchronizable(false)
+            .path("role-bindings")
+            .names(List.of("rolebindings", "rolebinding", "rb"))
+            .build();
+    public static final ResourceDefinition TOPIC = ResourceDefinition.builder()
+            .kind("Topic")
+            .namespaced(true)
+            .synchronizable(true)
+            .path("topics")
+            .names(List.of("topics", "topic", "to"))
+            .build();
+    public static final ResourceDefinition NAMESPACE = ResourceDefinition.builder()
+            .kind("Namespace")
                         .namespaced(false)
                         .synchronizable(false)
                         .path("namespaces")
                         .names(List.of("namespaces", "namespace", "ns"))
-                        .build()
+            .build();
+
+    @Get
+    public List<ResourceDefinition> list() {
+        return List.of(
+                ACL,
+                CONNECTOR,
+                ROLE_BINDING,
+                TOPIC,
+                NAMESPACE
                 );
     }
 
