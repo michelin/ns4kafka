241b954902db354eed718c567934e66eb9f33810
Twobeeb <chanaud.julien@gmail.com>
2021-10-16 16:44:54 +0200

    Added tests

--- api/src/test/java/com/michelin/ns4kafka/services/StreamServiceTest.java
+++ api/src/test/java/com/michelin/ns4kafka/services/StreamServiceTest.java
@@ -1,5 +1,6 @@
 package com.michelin.ns4kafka.services;
 
+import com.michelin.ns4kafka.models.AccessControlEntry;
 import com.michelin.ns4kafka.models.KafkaStream;
 import com.michelin.ns4kafka.models.Namespace;
 import com.michelin.ns4kafka.models.ObjectMeta;
@@ -142,6 +143,96 @@ public class StreamServiceTest {
 
     @Test
     void isNamespaceOwnerOfKafkaStream() {
+        Namespace ns = Namespace.builder()
+                .metadata(ObjectMeta.builder()
+                        .name("test")
+                        .cluster("local")
+                        .build())
+                .build();
 
+        AccessControlEntry ace1 = AccessControlEntry.builder()
+                .spec(AccessControlEntry.AccessControlEntrySpec.builder()
+                        .resourceType(AccessControlEntry.ResourceType.TOPIC)
+                        .resourcePatternType(AccessControlEntry.ResourcePatternType.PREFIXED)
+                        .permission(AccessControlEntry.Permission.OWNER)
+                        .resource("test.")
+                        .grantedTo("test")
+                        .build()
+                )
+                .build();
+        AccessControlEntry ace2 = AccessControlEntry.builder()
+                .spec(AccessControlEntry.AccessControlEntrySpec.builder()
+                        .resourceType(AccessControlEntry.ResourceType.GROUP)
+                        .resourcePatternType(AccessControlEntry.ResourcePatternType.PREFIXED)
+                        .permission(AccessControlEntry.Permission.OWNER)
+                        .resource("test.")
+                        .grantedTo("test")
+                        .build()
+                )
+                .build();
+        AccessControlEntry ace3 = AccessControlEntry.builder()
+                .spec(AccessControlEntry.AccessControlEntrySpec.builder()
+                        .resourceType(AccessControlEntry.ResourceType.CONNECT)
+                        .resourcePatternType(AccessControlEntry.ResourcePatternType.PREFIXED)
+                        .permission(AccessControlEntry.Permission.OWNER)
+                        .resource("test.")
+                        .grantedTo("test")
+                        .build()
+                )
+                .build();
+        AccessControlEntry ace4 = AccessControlEntry.builder()
+                .spec(AccessControlEntry.AccessControlEntrySpec.builder()
+                        .resourceType(AccessControlEntry.ResourceType.TOPIC)
+                        .resourcePatternType(AccessControlEntry.ResourcePatternType.LITERAL)
+                        .permission(AccessControlEntry.Permission.OWNER)
+                        .resource("test-bis.")
+                        .grantedTo("test")
+                        .build()
+                )
+                .build();
+        AccessControlEntry ace5 = AccessControlEntry.builder()
+                .spec(AccessControlEntry.AccessControlEntrySpec.builder()
+                        .resourceType(AccessControlEntry.ResourceType.GROUP)
+                        .resourcePatternType(AccessControlEntry.ResourcePatternType.LITERAL)
+                        .permission(AccessControlEntry.Permission.OWNER)
+                        .resource("test-bis.")
+                        .grantedTo("test")
+                        .build()
+                )
+                .build();
+        AccessControlEntry ace6 = AccessControlEntry.builder()
+                .spec(AccessControlEntry.AccessControlEntrySpec.builder()
+                        .resourceType(AccessControlEntry.ResourceType.GROUP)
+                        .resourcePatternType(AccessControlEntry.ResourcePatternType.PREFIXED)
+                        .permission(AccessControlEntry.Permission.OWNER)
+                        .resource("test-ter.")
+                        .grantedTo("test")
+                        .build()
+                )
+                .build();
+        AccessControlEntry ace7 = AccessControlEntry.builder()
+                .spec(AccessControlEntry.AccessControlEntrySpec.builder()
+                        .resourceType(AccessControlEntry.ResourceType.TOPIC)
+                        .resourcePatternType(AccessControlEntry.ResourcePatternType.PREFIXED)
+                        .permission(AccessControlEntry.Permission.OWNER)
+                        .resource("test-qua.")
+                        .grantedTo("test")
+                        .build()
+                )
+                .build();
+
+        when(accessControlEntryService.findAllGrantedToNamespace(ns))
+                .thenReturn(List.of(ace1, ace2, ace3, ace4, ace5, ace6, ace7));
+
+        Assertions.assertTrue(
+                streamService.isNamespaceOwnerOfKafkaStream(ns, "test.stream"));
+        Assertions.assertFalse(
+                streamService.isNamespaceOwnerOfKafkaStream(ns, "test-bis.stream"),"ACL are LITERAL");
+        Assertions.assertFalse(
+                streamService.isNamespaceOwnerOfKafkaStream(ns, "test-ter.stream"), "Topic ACL missing");
+        Assertions.assertFalse(
+                streamService.isNamespaceOwnerOfKafkaStream(ns, "test-qua.stream"),"Group ACL missing");
+        Assertions.assertFalse(
+                streamService.isNamespaceOwnerOfKafkaStream(ns, "test-nop.stream"),"No ACL");
     }
 }
