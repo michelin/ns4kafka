d9c852452a545e51616562f9e571a10c9aca3161
Julien Chanaud <chanaud.julien@gmail.com>
2021-11-29 10:56:39 +0100

    Implement SASL security on TestContainers (#133)
    * Implement SASL security on TestContainers
    
    * Activate ACL Integration tests
    
    * Merge Micronaut 3.1.4

--- api/src/test/java/com/michelin/ns4kafka/integration/AbstractIntegrationTest.java
+++ api/src/test/java/com/michelin/ns4kafka/integration/AbstractIntegrationTest.java
@@ -25,20 +25,56 @@ public abstract class AbstractIntegrationTest implements TestPropertyProvider {
         if (kafka == null || !kafka.isRunning()) {
             network = Network.newNetwork();
             kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:" + CONFLUENT_VERSION))
+                    .withEnv(
+                            "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP",
+                            "PLAINTEXT:SASL_PLAINTEXT,BROKER:SASL_PLAINTEXT"
+                    )
+                    .withEnv("KAFKA_INTER_BROKER_LISTENER_NAME", "BROKER")
+                    .withEnv("KAFKA_SASL_MECHANISM_INTER_BROKER_PROTOCOL", "PLAIN")
+                    .withEnv("KAFKA_LISTENER_NAME_PLAINTEXT_SASL_ENABLED_MECHANISMS", "PLAIN")
+                    .withEnv("KAFKA_LISTENER_NAME_BROKER_SASL_ENABLED_MECHANISMS", "PLAIN")
+
+                    .withEnv("KAFKA_LISTENER_NAME_BROKER_PLAIN_SASL_JAAS_CONFIG",
+                            "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                                    "username=\"admin\" password=\"admin\" " +
+                                    "user_admin=\"admin\" " +
+                                    "user_client=\"client\";")
+                    .withEnv("KAFKA_SASL_JAAS_CONFIG",
+                            "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                                    "username=\"admin\" password=\"admin\";")
+                    .withEnv(
+                            "KAFKA_LISTENER_NAME_PLAINTEXT_PLAIN_SASL_JAAS_CONFIG",
+                            "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                                    "username=\"admin\" password=\"admin\" " +
+                                    "user_admin=\"admin\" " +
+                                    "user_client=\"client\";")
+                    .withEnv("KAFKA_AUTHORIZER_CLASS_NAME", "kafka.security.auth.SimpleAclAuthorizer")
+                    .withEnv("KAFKA_SUPER_USERS", "User:admin")
                     .withNetworkAliases("kafka")
                     .withNetwork(network);
             kafka.start();
         }
 
         return Map.of(
-                "kafka.bootstrap.servers", kafka.getBootstrapServers(),
-                "ns4kafka.managed-clusters.test-cluster.config.bootstrap.servers", kafka.getBootstrapServers()
+                "kafka.bootstrap.servers", kafka.getHost()+":"+kafka.getMappedPort(9093),
+                "kafka.sasl.mechanism", "PLAIN",
+                "kafka.security.protocol", "SASL_PLAINTEXT",
+                "kafka.sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"admin\" password=\"admin\";",
+
+                "ns4kafka.managed-clusters.test-cluster.config.bootstrap.servers", kafka.getHost()+":"+kafka.getMappedPort(9093),
+                "ns4kafka.managed-clusters.test-cluster.config.sasl.mechanism", "PLAIN",
+                "ns4kafka.managed-clusters.test-cluster.config.security.protocol", "SASL_PLAINTEXT",
+                "ns4kafka.managed-clusters.test-cluster.config.sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"admin\" password=\"admin\";"
         );
     }
 
     public Admin getAdminClient() {
         if (adminClient == null)
-            adminClient = Admin.create(Map.of("bootstrap.servers", kafka.getBootstrapServers()));
+            adminClient = Admin.create(Map.of(
+                    "bootstrap.servers", kafka.getHost()+":"+kafka.getMappedPort(9093),
+                    "sasl.mechanism", "PLAIN",
+                    "security.protocol", "SASL_PLAINTEXT",
+                    "sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"admin\" password=\"admin\";"));
         return adminClient;
     }
 }
