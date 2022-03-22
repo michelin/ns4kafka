92059815dbba21a16ad0faab630cfc3aed46ec70
Loïc Greffier <loic.greffier_ext@michelin.com>
2022-03-22 18:43:07 +0100

    Check if user groups exist in any namespace before authentication

--- api/src/main/java/com/michelin/ns4kafka/security/gitlab/GitlabAuthenticationProvider.java
+++ api/src/main/java/com/michelin/ns4kafka/security/gitlab/GitlabAuthenticationProvider.java
@@ -1,6 +1,8 @@
 package com.michelin.ns4kafka.security.gitlab;
 
 import com.michelin.ns4kafka.security.ResourceBasedSecurityRule;
+import com.michelin.ns4kafka.security.SecurityConfig;
+import com.michelin.ns4kafka.services.RoleBindingService;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.security.authentication.*;
@@ -20,33 +22,64 @@ import java.util.Map;
 @Singleton
 public class GitlabAuthenticationProvider implements AuthenticationProvider {
 
+    /**
+     * The Gitlab authentication service
+     */
     @Inject
     GitlabAuthenticationService gitlabAuthenticationService;
 
+    /**
+     * The resource security service
+     */
     @Inject
     ResourceBasedSecurityRule resourceBasedSecurityRule;
 
+    /**
+     * The role binding service
+     */
+    @Inject
+    RoleBindingService roleBindingService;
+
+    /**
+     * The NS4Kafka security config service
+     */
+    @Inject
+    SecurityConfig securityConfig;
+
+    /**
+     * Perform user authentication with GitLab
+     *
+     * @param httpRequest The HTTP request
+     * @param authenticationRequest The authentication request
+     * @return An authentication response with the user details
+     */
     @Override
     public Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest, AuthenticationRequest<?,?> authenticationRequest) {
         Flowable<AuthenticationResponse> responseFlowable = Flowable.create(emitter -> {
             String token = authenticationRequest.getSecret().toString();
-            log.debug("Checking authentication with token : {}",token);
+
+            log.debug("Checking authentication with token: {}", token);
+
             try {
                 String username = gitlabAuthenticationService.findUsername(token).blockingGet();
                 List<String> groups = gitlabAuthenticationService.findAllGroups(token).toList().blockingGet();
 
-                UserDetails user = new UserDetails(username, resourceBasedSecurityRule.computeRolesFromGroups(groups), Map.of("groups", groups));
-
-                emitter.onNext(user);
-                emitter.onComplete();
-            }catch (Exception e){
-                log.debug("Exception during authenticate : {}", e.getMessage());
+                if (roleBindingService.listByGroups(groups).isEmpty() && !groups.contains(securityConfig.getAdminGroup())) {
+                    log.debug("Error during authentication: user groups not found in any namespace");
+                    emitter.onError(new AuthenticationException(new AuthenticationFailed("User groups not found in any namespace. There may be an error on the GitLab group of your namespace.")));
+                } else {
+                    UserDetails user = new UserDetails(username, resourceBasedSecurityRule.computeRolesFromGroups(groups), Map.of("groups", groups));
+                    emitter.onNext(user);
+                    emitter.onComplete();
+                }
+            } catch (Exception e){
+                log.debug("Exception during authentication: {}", e.getMessage());
                 emitter.onError(new AuthenticationException(new AuthenticationFailed(AuthenticationFailureReason.CREDENTIALS_DO_NOT_MATCH)));
             }
 
         }, BackpressureStrategy.ERROR);
-        responseFlowable = responseFlowable.subscribeOn(Schedulers.io());
-        return responseFlowable;
+
+        return responseFlowable.subscribeOn(Schedulers.io());
     }
 
 }
--- api/src/test/java/com/michelin/ns4kafka/security/GitlabAuthenticationProviderTest.java
+++ api/src/test/java/com/michelin/ns4kafka/security/GitlabAuthenticationProviderTest.java
@@ -1,7 +1,10 @@
 package com.michelin.ns4kafka.security;
 
+import com.michelin.ns4kafka.models.ObjectMeta;
+import com.michelin.ns4kafka.models.RoleBinding;
 import com.michelin.ns4kafka.security.gitlab.GitlabAuthenticationProvider;
 import com.michelin.ns4kafka.security.gitlab.GitlabAuthenticationService;
+import com.michelin.ns4kafka.services.RoleBindingService;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.security.authentication.*;
@@ -21,33 +24,72 @@ import java.util.List;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class GitlabAuthenticationProviderTest {
+class GitlabAuthenticationProviderTest {
+    /**
+     * The mocked GitLab authentication service
+     */
     @Mock
     GitlabAuthenticationService gitlabAuthenticationService;
+
+    /**
+     * The mocked resource security service
+     */
     @Mock
     ResourceBasedSecurityRule resourceBasedSecurityRule;
+
+    /**
+     * The role binding service
+     */
+    @Mock
+    RoleBindingService roleBindingService;
+
+    /**
+     * The NS4Kafka security config service
+     */
+    @Mock
+    SecurityConfig securityConfig;
+
+    /**
+     * The mocked Gitlab authentication provider
+     */
     @InjectMocks
     GitlabAuthenticationProvider gitlabAuthenticationProvider;
 
+    /**
+     * Assert the user authentication is successful
+     */
     @Test
-    public void authenticationSuccess(){
+    void authenticationSuccess() {
+        AuthenticationRequest<String, String> authenticationRequest = new UsernamePasswordCredentials("username","53cu23d_70k3n");
 
-        AuthenticationRequest authenticationRequest = new UsernamePasswordCredentials("username","53cu23d_70k3n");
         List<String> groups = List.of("group-1","group-2");
-        when(gitlabAuthenticationService.findUsername(authenticationRequest.getSecret().toString()))
+
+        RoleBinding roleBinding = RoleBinding.builder()
+                .metadata(ObjectMeta.builder()
+                        .name("namespace-rb1")
+                        .cluster("local")
+                        .build())
+                .spec(RoleBinding.RoleBindingSpec.builder()
+                        .subject(RoleBinding.Subject.builder()
+                                .subjectName("group-1")
+                                .subjectType(RoleBinding.SubjectType.GROUP)
+                                .build())
+                        .build())
+                .build();
+
+        when(gitlabAuthenticationService.findUsername(authenticationRequest.getSecret()))
                 .thenReturn(Maybe.just("email"));
-        when(gitlabAuthenticationService.findAllGroups(authenticationRequest.getSecret().toString()))
+        when(gitlabAuthenticationService.findAllGroups(authenticationRequest.getSecret()))
                 .thenReturn(Flowable.fromIterable(groups));
+        when(roleBindingService.listByGroups(groups))
+                .thenReturn(List.of(roleBinding));
         when(resourceBasedSecurityRule.computeRolesFromGroups(groups))
                 .thenReturn(List.of());
 
-        TestSubscriber<AuthenticationResponse> subscriber = new TestSubscriber();
+        TestSubscriber<AuthenticationResponse> subscriber = new TestSubscriber<>();
         Publisher<AuthenticationResponse> authenticationResponsePublisher = gitlabAuthenticationProvider.authenticate(null, authenticationRequest);
-
         authenticationResponsePublisher.subscribe(subscriber);
         subscriber.awaitTerminalEvent();
-
-        //then
 
         subscriber.assertComplete();
         subscriber.assertNoErrors();
@@ -64,25 +106,30 @@ public class GitlabAuthenticationProviderTest {
 
     }
 
+    /**
+     * Assert the admin authentication is successful
+     */
     @Test
-    public void authenticationSuccessAdmin(){
+    void authenticationSuccessAdmin() {
+        AuthenticationRequest<String, String> authenticationRequest = new UsernamePasswordCredentials("admin","53cu23d_70k3n");
 
-        AuthenticationRequest authenticationRequest = new UsernamePasswordCredentials("admin","53cu23d_70k3n");
         List<String> groups = List.of("group-1","group-2","group-admin");
-        when(gitlabAuthenticationService.findUsername(authenticationRequest.getSecret().toString()))
+        when(gitlabAuthenticationService.findUsername(authenticationRequest.getSecret()))
                 .thenReturn(Maybe.just("email"));
-        when(gitlabAuthenticationService.findAllGroups(authenticationRequest.getSecret().toString()))
+        when(gitlabAuthenticationService.findAllGroups(authenticationRequest.getSecret()))
                 .thenReturn(Flowable.fromIterable(groups));
+        when(roleBindingService.listByGroups(groups))
+                .thenReturn(List.of());
+        when(securityConfig.getAdminGroup())
+                .thenReturn("group-admin");
         when(resourceBasedSecurityRule.computeRolesFromGroups(groups))
                 .thenReturn(List.of(ResourceBasedSecurityRule.IS_ADMIN));
 
-        TestSubscriber<AuthenticationResponse> subscriber = new TestSubscriber();
+        TestSubscriber<AuthenticationResponse> subscriber = new TestSubscriber<>();
         Publisher<AuthenticationResponse> authenticationResponsePublisher = gitlabAuthenticationProvider.authenticate(null, authenticationRequest);
 
         authenticationResponsePublisher.subscribe(subscriber);
         subscriber.awaitTerminalEvent();
-
-        //then
 
         subscriber.assertComplete();
         subscriber.assertNoErrors();
@@ -96,26 +143,52 @@ public class GitlabAuthenticationProviderTest {
         Assertions.assertEquals("email", actualUserDetails.getUsername());
         Assertions.assertIterableEquals(groups, (List<String>)actualUserDetails.getAttributes("roles","username").get("groups"));
         Assertions.assertIterableEquals(List.of(ResourceBasedSecurityRule.IS_ADMIN), actualUserDetails.getRoles(),"User has custom roles");
-
     }
 
+    /**
+     * Assert the authentication fails when GitLab responds HTTP 403
+     */
     @Test
-    public void authenticationFailure(){
+    void authenticationFailure() {
+        AuthenticationRequest<String, String> authenticationRequest = new UsernamePasswordCredentials("admin","f4k3_70k3n");
 
-        AuthenticationRequest authenticationRequest = new UsernamePasswordCredentials("admin","f4k3_70k3n");
-        List<String> groups = List.of("group-1","group-2","group-admin");
-        when(gitlabAuthenticationService.findUsername(authenticationRequest.getSecret().toString()))
+        when(gitlabAuthenticationService.findUsername(authenticationRequest.getSecret()))
                 .thenThrow(new HttpClientResponseException("403 Unauthorized", HttpResponse.unauthorized()));
 
-        TestSubscriber<AuthenticationResponse> subscriber = new TestSubscriber();
+        TestSubscriber<AuthenticationResponse> subscriber = new TestSubscriber<>();
         Publisher<AuthenticationResponse> authenticationResponsePublisher = gitlabAuthenticationProvider.authenticate(null, authenticationRequest);
 
         authenticationResponsePublisher.subscribe(subscriber);
         subscriber.awaitTerminalEvent();
 
-        //then
         subscriber.assertError(AuthenticationException.class);
         subscriber.assertValueCount(0);
+    }
 
+    /**
+     * Assert the authentication fails when GitLab responds HTTP 403
+     */
+    @Test
+    void authenticationFailureGroupsNotFound() {
+        AuthenticationRequest<String, String> authenticationRequest = new UsernamePasswordCredentials("admin","f4k3_70k3n");
+
+        List<String> groups = List.of("group-1","group-2");
+        when(gitlabAuthenticationService.findUsername(authenticationRequest.getSecret()))
+                .thenReturn(Maybe.just("email"));
+        when(gitlabAuthenticationService.findAllGroups(authenticationRequest.getSecret()))
+                .thenReturn(Flowable.fromIterable(groups));
+        when(roleBindingService.listByGroups(groups))
+                .thenReturn(List.of());
+        when(securityConfig.getAdminGroup())
+                .thenReturn("group-admin");
+
+        TestSubscriber<AuthenticationResponse> subscriber = new TestSubscriber<>();
+        Publisher<AuthenticationResponse> authenticationResponsePublisher = gitlabAuthenticationProvider.authenticate(null, authenticationRequest);
+
+        authenticationResponsePublisher.subscribe(subscriber);
+        subscriber.awaitTerminalEvent();
+
+        subscriber.assertError(AuthenticationException.class);
+        subscriber.assertValueCount(0);
     }
 }
