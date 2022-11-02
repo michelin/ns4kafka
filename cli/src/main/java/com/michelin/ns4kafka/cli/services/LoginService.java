52794d0ef93ad1c2121c22d8e4dee589b982c367
Loïc Greffier <loic.greffier_ext@michelin.com>
2022-11-02 10:59:28 +0100

    Displayed information when relogging with CLI

--- cli/src/main/java/com/michelin/ns4kafka/cli/services/LoginService.java
+++ cli/src/main/java/com/michelin/ns4kafka/cli/services/LoginService.java
@@ -21,9 +21,13 @@ public class LoginService {
     private final KafkactlConfig kafkactlConfig;
     private final ClusterResourceClient clusterResourceClient;
     private final File jwtFile;
-
     private String accessToken = null;
 
+    /**
+     * Constructor
+     * @param kafkactlConfig The Kafkactl config
+     * @param clusterResourceClient The client for resources
+     */
     public LoginService(KafkactlConfig kafkactlConfig, ClusterResourceClient clusterResourceClient) {
         this.kafkactlConfig = kafkactlConfig;
         this.clusterResourceClient = clusterResourceClient;
@@ -45,27 +49,24 @@ public class LoginService {
 
     public boolean isAuthenticated() {
         try {
-            // 0. JWT token file exists
-            if (!jwtFile.exists())
+            if (!jwtFile.exists()) {
                 return false;
-            // 1. Open local JWT token file
+            }
+
             ObjectMapper objectMapper = new ObjectMapper();
             BearerAccessRefreshToken token = objectMapper.readValue(jwtFile, BearerAccessRefreshToken.class);
-            // 2. Verify token against ns4kafka /user_info endpoint
             UserInfoResponse userInfo = clusterResourceClient.tokenInfo("Bearer " + token.getAccessToken());
-            // 3. Display token result
             if (KafkactlCommand.VERBOSE) {
                 Date expiry = new Date(userInfo.getExp() * 1000);
                 System.out.println("Authentication reused, welcome " + userInfo.getUsername() + "!");
                 System.out.println("Your session is valid until " + expiry);
             }
+
             accessToken = token.getAccessToken();
             return userInfo.isActive();
         } catch (IOException e) {
-            // File doesn't exist or File issue or JSON parsing issue
             System.out.println("Unexpected error occurred: " + e.getMessage());
         } catch (HttpClientResponseException e) {
-            //401 UNAUTHORIZED OR anything > 400
             if (e.getStatus() != HttpStatus.UNAUTHORIZED) {
                 System.out.println("Unexpected error occurred: " + e.getMessage());
             }
@@ -75,32 +76,30 @@ public class LoginService {
 
     public boolean login(String user, String password) {
         try {
-            // 1. Call ns4kafka /login
-            BearerAccessRefreshToken tokenResponse =
-                    clusterResourceClient.login(
-                            UsernameAndPasswordRequest
-                                    .builder()
-                                    .username(user)
-                                    .password(password)
-                                    .build()
-                    );
-            // 2. Store token in memory;
+            System.out.print("Authenticating... ");
+            BearerAccessRefreshToken tokenResponse = clusterResourceClient.login(UsernameAndPasswordRequest
+                            .builder()
+                            .username(user)
+                            .password(password)
+                            .build());
+
             accessToken = tokenResponse.getAccessToken();
-            // 3. Display token result
+
             if (KafkactlCommand.VERBOSE) {
-                Calendar calendar = Calendar.getInstance(); // gets a calendar using the default time zone and locale.
+                Calendar calendar = Calendar.getInstance();
                 calendar.add(Calendar.SECOND, tokenResponse.getExpiresIn());
                 System.out.println("Authentication successful, welcome " + tokenResponse.getUsername() + "!");
-                System.out.println("Your session is valid until " + calendar.getTime());
+                System.out.println("Your session is valid until " + calendar.getTime() + ".");
             }
-            // 4. Store token result locally
+
             try {
                 ObjectMapper objectMapper = new ObjectMapper();
                 objectMapper.writeValue(jwtFile, tokenResponse);
             } catch (IOException e) {
-                System.out.println("WARNING : Unexpected error occurred: " + e.getMessage());
+                System.out.println("Unexpected error occurred: " + e.getMessage());
             }
 
+            System.out.println("Done.");
             return true;
         } catch (HttpClientResponseException e) {
             System.out.println("Authentication failed with message: " + e.getMessage());
@@ -112,7 +111,7 @@ public class LoginService {
     /**
      * If exists, delete JWT file
      */
-    public void deleteJWTfile() {
+    public void deleteJWTFile() {
         if (jwtFile.exists()) {
             jwtFile.delete();
         }
