b3bc092bce539fbebd57d793de55513e5b4862a7
Loïc Greffier <loic.greffier_ext@michelin.com>
2023-07-13 17:27:20 +0200

    Fix tests

--- src/test/java/com/michelin/ns4kafka/services/NamespaceServiceTest.java
+++ src/test/java/com/michelin/ns4kafka/services/NamespaceServiceTest.java
@@ -6,7 +6,6 @@ import com.michelin.ns4kafka.models.*;
 import com.michelin.ns4kafka.models.Namespace.NamespaceSpec;
 import com.michelin.ns4kafka.models.connector.Connector;
 import com.michelin.ns4kafka.repositories.NamespaceRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,6 +17,7 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -128,7 +128,6 @@ class NamespaceServiceTest {
 
     @Test
     void validateCreationANamespaceAlreadyExistsSuccess() {
-
         Namespace ns = Namespace.builder()
                 .metadata(ObjectMeta.builder()
                         .name("namespace")
