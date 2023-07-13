a77dc99f5db9117dab0f3417ee49b962a47cb525
Loïc Greffier <loic.greffier_ext@michelin.com>
2023-07-13 17:17:24 +0200

    Fix tests

--- src/test/java/com/michelin/ns4kafka/services/ConnectorServiceTest.java
+++ src/test/java/com/michelin/ns4kafka/services/ConnectorServiceTest.java
@@ -33,7 +33,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
