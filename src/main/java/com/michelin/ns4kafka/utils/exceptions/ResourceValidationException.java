b396b4b274a2218e0a9a2bc5e17966caa25372d0
Loïc Greffier <loic.greffier@hotmail.fr>
2024-02-19 20:07:56 +0100

    Set all args constructor

--- src/main/java/com/michelin/ns4kafka/utils/exceptions/ResourceValidationException.java
+++ src/main/java/com/michelin/ns4kafka/utils/exceptions/ResourceValidationException.java
@@ -2,21 +2,23 @@ package com.michelin.ns4kafka.utils.exceptions;
 
 import java.io.Serial;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * Resource validation exception.
  */
 @Getter
+@AllArgsConstructor
 public class ResourceValidationException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 32400191899153204L;
 
-    private final List<String> validationErrors;
-
     private final String kind;
 
     private final String name;
+
+    private final List<String> validationErrors;
 
     /**
      * Constructor.
@@ -29,18 +31,5 @@ public class ResourceValidationException extends RuntimeException {
         this.kind = kind;
         this.name = name;
         this.validationErrors = List.of(validationError);
-    }
-
-    /**
-     * Constructor.
-     *
-     * @param kind             The kind of the resource
-     * @param name             The name of the resource
-     * @param validationErrors The validation errors
-     */
-    public ResourceValidationException(String kind, String name, List<String> validationErrors) {
-        this.kind = kind;
-        this.name = name;
-        this.validationErrors = validationErrors;
     }
 }
