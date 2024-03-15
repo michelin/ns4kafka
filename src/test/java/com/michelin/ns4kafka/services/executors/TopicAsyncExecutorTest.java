b5b4e3d1c376ef7cde1a23e5c50af87b9d1f175f
thcai <thomas.cai_ext@michelin.com>
2024-03-15 18:07:04 +0100

    unused import

--- src/test/java/com/michelin/ns4kafka/services/executors/TopicAsyncExecutorTest.java
+++ src/test/java/com/michelin/ns4kafka/services/executors/TopicAsyncExecutorTest.java
@@ -8,8 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
