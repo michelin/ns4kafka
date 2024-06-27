d50525ed168c61f26f61e9623fc3d13692033a49
Thomas CAI <92149044+ThomasCAI-mlv@users.noreply.github.com>
2024-06-27 16:25:43 +0200

    Add the possibility to configure Confluent Stream catalog limit (#402)
    * Set stream catalog API request limit to 500 & make the limit a parameter of the application
    
    * Fix syntax
    
    * Simplify properties ref
    
    * Remove extra line
    
    ---------
    
    Co-authored-by: thcai <thomas.cai_ext@michelin.com>

---
+++ src/main/java/com/michelin/ns4kafka/property/ConfluentCloudProperties.java
@@ -0,0 +1,25 @@
+package com.michelin.ns4kafka.property;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Confluent Cloud properties.
+ */
+@Getter
+@Setter
+@ConfigurationProperties("ns4kafka.confluent-cloud")
+public class ConfluentCloudProperties {
+    private StreamCatalogProperties streamCatalog;
+
+    /**
+     * Stream Catalog properties.
+     */
+    @Getter
+    @Setter
+    @ConfigurationProperties("stream-catalog")
+    public static class StreamCatalogProperties {
+        int pageSize = 500;
+    }
+}
