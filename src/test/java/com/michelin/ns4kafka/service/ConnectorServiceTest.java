aa117f651b187332003c05e613d7ce4b835e3b38
thcai <thomas.cai_ext@michelin.com>
2024-07-29 09:54:12 +0200

    fix unused import

--- src/test/java/com/michelin/ns4kafka/service/ConnectorServiceTest.java
+++ src/test/java/com/michelin/ns4kafka/service/ConnectorServiceTest.java
@@ -1,7 +1,6 @@
 package com.michelin.ns4kafka.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
