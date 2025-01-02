a0127537d3039c26649826e9c16f83bdbceef355
Loïc Greffier <loic.greffier@michelin.com>
2025-01-02 15:54:17 +0100

    Fix checkstyle

--- src/test/java/com/michelin/ns4kafka/integration/ConfigIntegrationTest.java
+++ src/test/java/com/michelin/ns4kafka/integration/ConfigIntegrationTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 @MicronautTest
-public class ConfigIntegrationTest extends KafkaIntegrationTest {
+class ConfigIntegrationTest extends KafkaIntegrationTest {
 
     @Inject
     List<ManagedClusterProperties> managedClusterProperties;
