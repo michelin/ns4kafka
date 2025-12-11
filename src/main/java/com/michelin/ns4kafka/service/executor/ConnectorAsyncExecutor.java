29c556c593d7ba9853f7bebdb43f58e537977847
Thomas CAI <92149044+ThomasCAI-mlv@users.noreply.github.com>
2025-12-11 21:18:54 +0100

    Fix infinite connector synchronisation with masked sensitive field values (#673)
    * Fix connector synchronisation with masked field value
    
    * Add unit tests
    
    * Fix issues & increase coverage
    
    * Remove from configuration
    
    * Remove stub from test
    
    * Spotless apply
    
    * Make field static
    
    * Remove unnecessary code
    
    * Respect naming convention
    
    * Spotless

--- src/main/java/com/michelin/ns4kafka/service/executor/ConnectorAsyncExecutor.java
+++ src/main/java/com/michelin/ns4kafka/service/executor/ConnectorAsyncExecutor.java
@@ -31,6 +31,7 @@ import com.michelin.ns4kafka.service.client.connect.entities.ConnectorStatus;
 import io.micronaut.context.annotation.EachBean;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import jakarta.inject.Singleton;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -47,6 +48,8 @@ import reactor.core.publisher.Mono;
 @Singleton
 @AllArgsConstructor
 public class ConnectorAsyncExecutor {
+    private static final String SENSITIVE_FIELD_MASK = "••••••••••••";
+
     private final Set<String> healthyConnectClusters = new HashSet<>();
     private final Set<String> idleConnectClusters = new HashSet<>();
 
@@ -179,15 +182,17 @@ public class ConnectorAsyncExecutor {
                             .toList();
 
                     List<Connector> toUpdate = ns4kafkaConnectors.stream()
-                            .filter(connector -> brokerConnectors.stream().anyMatch(connector1 -> {
-                                if (connector1
-                                        .getMetadata()
-                                        .getName()
-                                        .equals(connector.getMetadata().getName())) {
-                                    return !connectorsAreSame(connector, connector1);
-                                }
-                                return false;
-                            }))
+                            .filter(connector -> (connector.getStatus() != null
+                                            && connector.getStatus().getToDeploy())
+                                    || brokerConnectors.stream().anyMatch(connector1 -> {
+                                        if (connector1
+                                                .getMetadata()
+                                                .getName()
+                                                .equals(connector.getMetadata().getName())) {
+                                            return !connectorsAreSame(connector, connector1);
+                                        }
+                                        return false;
+                                    }))
                             .toList();
 
                     if (!toCreate.isEmpty()) {
@@ -285,7 +290,7 @@ public class ConnectorAsyncExecutor {
      * @param actual The second connector
      * @return true it they are, false otherwise
      */
-    private boolean connectorsAreSame(Connector expected, Connector actual) {
+    boolean connectorsAreSame(Connector expected, Connector actual) {
         Map<String, String> expectedMap = expected.getSpec().getConfig();
         Map<String, String> actualMap = actual.getSpec().getConfig();
 
@@ -293,9 +298,18 @@ public class ConnectorAsyncExecutor {
             return false;
         }
 
-        return expectedMap.entrySet().stream()
-                .allMatch(e -> (e.getValue() == null && actualMap.get(e.getKey()) == null)
-                        || (e.getValue() != null && e.getValue().equals(actualMap.get(e.getKey()))));
+        return actualMap.entrySet().stream()
+                .allMatch(e -> (e.getValue() == null && expectedMap.get(e.getKey()) == null)
+                        // Password fields are masked when returned by Connect API in cp 7.9.3+
+                        // so these fields mess up the comparison
+                        // 2 solutions:
+                        // 1) Check with Connect API which field is masked and ignore the
+                        // comparison for the concerned fields
+                        // 2) Store the mask string and ignore the comparison when the "actual"
+                        // value corresponds to this mask
+                        // Solution 2 is chosen since it's simpler and has no additional API calls
+                        || (Arrays.asList(expectedMap.get(e.getKey()), SENSITIVE_FIELD_MASK)
+                                .contains(e.getValue())));
     }
 
     /**
@@ -312,11 +326,14 @@ public class ConnectorAsyncExecutor {
                         ConnectorSpecs.builder()
                                 .config(connector.getSpec().getConfig())
                                 .build())
-                .doOnSuccess(httpResponse -> log.info(
-                        "Success deploying connector {} on Kafka Connect {} of Kafka cluster {}.",
-                        connector.getMetadata().getName(),
-                        connector.getSpec().getConnectCluster(),
-                        managedClusterProperties.getName()))
+                .doOnSuccess(httpResponse -> {
+                    connector.getStatus().setToDeploy(false);
+                    log.info(
+                            "Success deploying connector {} on Kafka Connect {} of Kafka cluster {}.",
+                            connector.getMetadata().getName(),
+                            connector.getSpec().getConnectCluster(),
+                            managedClusterProperties.getName());
+                })
                 .doOnError(httpError -> log.error(
                         "Error deploying connector {} on Kafka Connect {} of Kafka cluster {}: {}",
                         connector.getMetadata().getName(),
---
+++ src/test/java/com/michelin/ns4kafka/service/executor/ConnectorAsyncExecutorTest.java
@@ -0,0 +1,134 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.michelin.ns4kafka.service.executor;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.michelin.ns4kafka.model.Metadata;
+import com.michelin.ns4kafka.model.connector.Connector;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ConnectorAsyncExecutorTest {
+    private static final String CONNECTOR_NAME = "myConnector";
+    private static final String SENSITIVE_FIELD_MASK = "••••••••••••";
+
+    @InjectMocks
+    ConnectorAsyncExecutor connectorAsyncExecutor;
+
+    @Test
+    void shouldCompareSameConnectors() {
+        Connector expectedConnector = Connector.builder()
+                .metadata(Metadata.builder().name(CONNECTOR_NAME).build())
+                .spec(Connector.ConnectorSpec.builder()
+                        .config(Map.of("config1", "value1", "config2", "value2"))
+                        .build())
+                .build();
+
+        Connector actualConnector = Connector.builder()
+                .metadata(Metadata.builder().name(CONNECTOR_NAME).build())
+                .spec(Connector.ConnectorSpec.builder()
+                        .config(Map.of("config1", "value1", "config2", "value2"))
+                        .build())
+                .build();
+
+        assertTrue(connectorAsyncExecutor.connectorsAreSame(expectedConnector, actualConnector));
+    }
+
+    @Test
+    void shouldCompareConnectorsWithDifferentNumberOfConfigs() {
+        Connector expectedConnector = Connector.builder()
+                .metadata(Metadata.builder().name(CONNECTOR_NAME).build())
+                .spec(Connector.ConnectorSpec.builder()
+                        .config(Map.of("config1", "value1"))
+                        .build())
+                .build();
+
+        Connector actualConnector = Connector.builder()
+                .metadata(Metadata.builder().name(CONNECTOR_NAME).build())
+                .spec(Connector.ConnectorSpec.builder()
+                        .config(Map.of("config1", "value1", "config2", "value2"))
+                        .build())
+                .build();
+
+        assertFalse(connectorAsyncExecutor.connectorsAreSame(expectedConnector, actualConnector));
+    }
+
+    @Test
+    void shouldCompareConnectorsWithDifferentConfigValues() {
+        Connector expectedConnector = Connector.builder()
+                .metadata(Metadata.builder().name(CONNECTOR_NAME).build())
+                .spec(Connector.ConnectorSpec.builder()
+                        .config(Map.of("config1", "differentValue1", "config2", "value2"))
+                        .build())
+                .build();
+
+        Connector actualConnector = Connector.builder()
+                .metadata(Metadata.builder().name(CONNECTOR_NAME).build())
+                .spec(Connector.ConnectorSpec.builder()
+                        .config(Map.of("config1", "value1", "config2", "value2"))
+                        .build())
+                .build();
+
+        assertFalse(connectorAsyncExecutor.connectorsAreSame(expectedConnector, actualConnector));
+    }
+
+    @Test
+    void shouldCompareConnectorsWithDifferentConfigKeys() {
+        Connector expectedConnector = Connector.builder()
+                .metadata(Metadata.builder().name(CONNECTOR_NAME).build())
+                .spec(Connector.ConnectorSpec.builder()
+                        .config(Map.of("differentConfig1", "value1", "config2", "value2"))
+                        .build())
+                .build();
+
+        Connector actualConnector = Connector.builder()
+                .metadata(Metadata.builder().name(CONNECTOR_NAME).build())
+                .spec(Connector.ConnectorSpec.builder()
+                        .config(Map.of("config1", "value1", "config2", "value2"))
+                        .build())
+                .build();
+
+        assertFalse(connectorAsyncExecutor.connectorsAreSame(expectedConnector, actualConnector));
+    }
+
+    @Test
+    void shouldCompareConnectorsWithMaskedConfigFromConnect() {
+        Connector expectedConnector = Connector.builder()
+                .metadata(Metadata.builder().name(CONNECTOR_NAME).build())
+                .spec(Connector.ConnectorSpec.builder()
+                        .config(Map.of("config1", "value1", "config2", "value2"))
+                        .build())
+                .build();
+
+        Connector actualConnector = Connector.builder()
+                .metadata(Metadata.builder().name(CONNECTOR_NAME).build())
+                .spec(Connector.ConnectorSpec.builder()
+                        .config(Map.of("config1", SENSITIVE_FIELD_MASK, "config2", "value2"))
+                        .build())
+                .build();
+
+        assertTrue(connectorAsyncExecutor.connectorsAreSame(expectedConnector, actualConnector));
+    }
+}
