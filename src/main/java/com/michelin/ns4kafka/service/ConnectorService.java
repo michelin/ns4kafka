24844abfc44137340293c507de78fa5a585f4887
AfrinKhan02 <amazingafrin@gmail.com>
2026-05-22 15:11:54 +0530

    Handle connector service & Return Kafka Connect reset offsets response

--- src/main/java/com/michelin/ns4kafka/service/ConnectorService.java
+++ src/main/java/com/michelin/ns4kafka/service/ConnectorService.java
@@ -28,6 +28,7 @@ import com.michelin.ns4kafka.model.Namespace;
 import com.michelin.ns4kafka.model.connect.Connector;
 import com.michelin.ns4kafka.repository.ConnectorRepository;
 import com.michelin.ns4kafka.service.client.connect.KafkaConnectClient;
+import com.michelin.ns4kafka.service.client.connect.entities.ConnectorOffsetsResponse;
 import com.michelin.ns4kafka.service.client.connect.entities.ConnectorSpecs;
 import com.michelin.ns4kafka.service.executor.ConnectorAsyncExecutor;
 import com.michelin.ns4kafka.util.FormatErrorUtils;
@@ -411,5 +412,29 @@ public class ConnectorService {
 
                     return HttpResponse.accepted();
                 });
+    }
+
+        /**
+         * Reset offsets for a given connector.
+         *
+         * @param namespace The namespace
+         * @param connector The connector
+         * @return An HTTP response
+         */
+        public Mono<HttpResponse<ConnectorOffsetsResponse>> resetOffsets(Namespace namespace, Connector connector) {
+                return kafkaConnectClient
+                                .resetOffsets(
+                                                namespace.getMetadata().getCluster(),
+                                                connector.getSpec().getConnectCluster(),
+                                                connector.getMetadata().getName())
+                                .map(response -> {
+                                        log.info(
+                                                        "Success resetting offsets for Connector [{}] on Namespace [{}] Connect [{}]",
+                                                        connector.getMetadata().getName(),
+                                                        namespace.getMetadata().getName(),
+                                                        connector.getSpec().getConnectCluster());
+
+                                        return HttpResponse.status(response.getStatus()).body(response.body());
+                                });
     }
 }
